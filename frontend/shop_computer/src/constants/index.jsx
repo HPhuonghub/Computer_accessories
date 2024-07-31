a502e3e8f6adef7dbd8e1f3935aaa591da1cf385
B20DCCN520_Hoàng Duy Phương <hoangduyphuong30102002@gmail.com>
2024-07-31 10:03:34 +0700

    Update files

--- frontend/shop_computer/src/constants/index.jsx
+++ frontend/shop_computer/src/constants/index.jsx
@@ -10,3 +10,8 @@ export const OAUTH2_REDIRECT_URI = keys.redirectUri;
 
 export const GOOGLE_AUTH_URL =
   API_BASE_URL + "/oauth2/authorize/google?redirect_uri=" + OAUTH2_REDIRECT_URI;
+
+export const FACEBOOK_AUTH_URL =
+  API_BASE_URL +
+  "/oauth2/authorize/facebook?redirect_uri=" +
+  OAUTH2_REDIRECT_URI;
